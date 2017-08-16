e2b3ac427ce9bc962b76944b967dc6ca10607996
Damien <dasseya1@gmail.com>
2017-08-16 16:39:50 -0400

    Imported BrowserRouter and Route

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { BrowserRouter, Route } from 'react-route-dom';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
