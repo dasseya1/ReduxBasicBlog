36ad7fa20b1c0dcada38ae5331ec5ea575214fdc
Damien <dasseya1@gmail.com>
2017-08-16 18:29:29 -0400

    Implemented fetch posts with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
+import axios from 'axios';
+
 export const FETCH_POSTS = 'fetch_posts';
 
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
+const API_URL = '?key=PAPERCLIP1234';
+
 export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts`);
     return {
         type: FETCH_POSTS
     }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-route-dom';
 import reducers from './reducers';
+import promise from 'redux-promise';
 import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
