5621458c140f851e2565bead88c6fab27884b116
Damien <dasseya1@gmail.com>
2017-08-17 11:26:14 -0400

    Wrote functions to validate input values

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -37,6 +37,21 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {};
+    if (!values.title) {
+        errors.title = "Enter a title!";
+    }
+    if (!values.categories) {
+        errors.categories = "Enter some categories!";
+    }
+    if (!values.content) {
+        errors.content = "Enter some content please!";
+    }
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
