e38ddec02f2f91770def306b9af36b446249b31f
Damien <dasseya1@gmail.com>
2017-08-18 07:07:12 -0400

    Selecting from ownProps

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -4,15 +4,26 @@ import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
     componentDidMount() {
-        this.props.fetchPost();
+        const { id } = this.props.match.params;
+        this.props.fetchPost(id);
     }
     render() {
+        const { post } = this.props;
+        if (!post) {
+            return <div>Loading ...</div>;
+        }
+
         return (
             <div>
-                Post Show!
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
     }
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+function mapStateToProps({ posts }, ownProps) {
+    return { post: posts[ownProps.match.params.id] };
+}
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
