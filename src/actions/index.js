ac8e23ed55e62410ca5d7c8c3b187458fb0d1398
Damien <dasseya1@gmail.com>
2017-08-18 07:40:05 -0400

    Functionality to cache records

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const CREATE_POST = 'create_post';
 export const FETCH_POST = 'fetch_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=PAPERC1234';
+const API_KEY = '?key=PAPERCLIP1234';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
